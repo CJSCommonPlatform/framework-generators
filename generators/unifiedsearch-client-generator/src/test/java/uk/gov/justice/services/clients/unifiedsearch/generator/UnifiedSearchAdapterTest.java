47b5e02b655d65fca265df68c916812a1c31b8cd
Shaun Francis <shaun@mapingo.co.uk>
2020-04-22 11:12:52 +0100

    Change UnifiedSearchAdapter to pass envelope to the UnifiedSearchIndexer

--- generators/unifiedsearch-client-generator/src/test/java/uk/gov/justice/services/clients/unifiedsearch/generator/UnifiedSearchAdapterTest.java
+++ generators/unifiedsearch-client-generator/src/test/java/uk/gov/justice/services/clients/unifiedsearch/generator/UnifiedSearchAdapterTest.java
@@ -1,11 +1,20 @@
 package uk.gov.justice.services.clients.unifiedsearch.generator;
 
-import static org.mockito.Answers.*;
+import static java.util.UUID.randomUUID;
+import static javax.json.Json.createObjectBuilder;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static uk.gov.justice.services.messaging.JsonEnvelope.envelopeFrom;
+import static uk.gov.justice.services.messaging.JsonEnvelope.metadataBuilder;
+import static uk.gov.justice.services.test.utils.core.matchers.JsonEnvelopeMetadataMatcher.withMetadataEnvelopedFrom;
 
 import uk.gov.justice.services.clients.unifiedsearch.core.UnifiedSearchTransformerCache;
+import uk.gov.justice.services.messaging.Envelope;
 import uk.gov.justice.services.messaging.JsonEnvelope;
+import uk.gov.justice.services.messaging.Metadata;
 import uk.gov.justice.services.unifiedsearch.TransformerApi;
 import uk.gov.justice.services.unifiedsearch.UnifiedSearchIndexer;
 
@@ -13,22 +22,14 @@ import javax.json.JsonObject;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Answers;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UnifiedSearchAdapterTest {
-
-    @Mock(answer = RETURNS_DEEP_STUBS)
-    private JsonEnvelope jsonEnvelope;
-
-    @Mock
-    private JsonObject transformedEvent;
-
-    @Mock
-    private UnifiedSearchIndexer unifiedSearchIndexer;
 
     @Mock
     private TransformerApi transformerApi;
@@ -39,17 +40,36 @@ public class UnifiedSearchAdapterTest {
     @InjectMocks
     private UnifiedSearchAdapter unifiedSearchAdapter;
 
+    @Captor
+    private ArgumentCaptor<Envelope<JsonObject>> envelopeArgumentCaptor;
+
     @Test
     public void verifyIndexerIsCalled() {
-        final String transformerOperations = "";
-        final String eventName = "test";
 
-        when(jsonEnvelope.metadata().name()).thenReturn(eventName);
+        final String eventName = "test";
+        final String transformerOperations = "";
+
+        final Metadata metadata = metadataBuilder()
+                .withId(randomUUID())
+                .withName(eventName)
+                .build();
+
+        final JsonEnvelope jsonEnvelope = envelopeFrom(
+                metadata,
+                createObjectBuilder().build());
+
+        final UnifiedSearchIndexer unifiedSearchIndexer = mock(UnifiedSearchIndexer.class);
+        final JsonObject transformedEvent = createObjectBuilder().add("transformed", "event").build();
+
         when(unifiedSearchTransformerCache.getTransformerConfigBy(eventName)).thenReturn(transformerOperations);
         when(transformerApi.transformWithJolt(transformerOperations, jsonEnvelope.payloadAsJsonObject())).thenReturn(transformedEvent);
 
         unifiedSearchAdapter.index(jsonEnvelope, unifiedSearchIndexer);
 
-        verify(unifiedSearchIndexer).indexData(transformedEvent);
+        verify(unifiedSearchIndexer).indexData(envelopeArgumentCaptor.capture());
+
+        final Envelope<JsonObject> jsonObjectEnvelope = envelopeArgumentCaptor.getValue();
+        assertThat(jsonObjectEnvelope.metadata(), withMetadataEnvelopedFrom(jsonEnvelope));
+        assertThat(jsonObjectEnvelope.payload(), is(transformedEvent));
     }
 }
